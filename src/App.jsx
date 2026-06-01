a67a8a044f6e96f773a20eab23e6f831872d4cd8
YenaKC <yenakim242@icloud.com>
2026-06-01 07:57:37 +0200

    Opinion of Yena in creating Update()

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import recipeData from './assets/data/recipes.json';
 
 export default function App() {
 
-
+// ALL RECIPE LIST
   const [currentRecipeList, setCurrentRecipeList] = useState(recipeData);
   const deleteRecipe = (id) => {
     const newList = currentRecipeList.filter((recipe) => {
@@ -34,9 +34,12 @@ export default function App() {
     setCurrentRecipeList(newRecipeAddedList)
   }
 
+  // UPDATE RECIPE : if the user want Update the recipe, execute onUpdate
   const updateRecipe = (updatedRecipeObj) => {
     const newUpdatedList = currentRecipeList.map((recipe) => {
-      return recipe.id === updatedRecipeObj.id ? updatedRecipeObj : recipe
+      return recipe.id === updatedRecipeObj.id // if the id exist on the database already, change to the edited recipe
+      ? updatedRecipeObj 
+      : recipe
     })
     setCurrentRecipeList(newUpdatedList)
   }
@@ -53,12 +56,14 @@ export default function App() {
             <RecipeList recipesList={currentRecipeList} onDelete={deleteRecipe} />
           } />
           <Route path='/about' element={<AboutPage />} />
+          
+          {/* CREATE/UPDATE/DETAILS/DELETE ON DASHBOARD  */}
           <Route
-            path='/dashboard/:recipeId?'
+            path='/dashboard/:recipeId?' //'..recipeID?' has possiblity both /dashboard and /dashboard/:recipeId
             element={
               <DashboardPage
                 onSubmit={addNewRecipe}
-                onUpdate={updateRecipe}
+                onUpdate={updateRecipe} 
                 recipesList={currentRecipeList} />}
           />
           <Route path='/recipe/:recipeId' element={<RecipeDetailsPage recipesList={currentRecipeList} />} />
--- src/pages/RecipeDetailsPage.jsx
+++ src/pages/RecipeDetailsPage.jsx
@@ -7,9 +7,15 @@ export const RecipeDetailsPage = ({ recipesList }) => {
 
     const recipe = recipesList.find((recipe) => {
         return recipe.id === recipeId;
-    })
+    });
     console.log(recipeId)
     console.log(recipe)
+
+    // Yena: When the user access to a recipe id that is not existed(already the card deleted or put different id), it could show the message, not 'undefined'. But we could put it or if you don't think it's not important, not.
+    if (!recipe) {
+        return <p>Recipe not found</p>;
+    }
+
     return (
         <>
             <div className='recipe-details-container'>
